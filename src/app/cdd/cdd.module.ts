d1ddfadb78b54c6333e84f89dc39cdb991abb8fb
akyrasouza <akyrapop@hotmail.com>
2022-01-10 14:00:41 -0300

    Adição de cartões e toolbar no módulo  CDD

--- src/app/cdd/cdd.module.ts
+++ src/app/cdd/cdd.module.ts
@@ -1,6 +1,11 @@
+//Módulos Externos
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { CddRoutingModule } from './cdd-routing.module';
 import { ClassesComponent } from './classes/classes.component';
@@ -13,7 +18,11 @@ import { ClassesComponent } from './classes/classes.component';
   imports: [
     CommonModule,
     CddRoutingModule,
-    MatTableModule
+    MatTableModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ]
 })
 export class CddModule { }
