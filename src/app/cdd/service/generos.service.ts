8c5a7261ef489712ac68b871da1695b3a98e6bd3
akyrasouza <akyrapop@hotmail.com>
2022-01-14 12:01:38 -0300

    Adição de ng-template

--- src/app/cdd/service/generos.service.ts
+++ src/app/cdd/service/generos.service.ts
@@ -1,6 +1,7 @@
 import { Generos } from './../modelos/generos';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { delay } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,9 @@ export class GenerosService {
   constructor(private clienteDados: HttpClient) { }
 
   listagemGeneros() {
-    return this.clienteDados.get<Generos[]>(this.urlAPI)
+    return this.clienteDados.get<Generos[]>(this.urlAPI).
+    pipe(
+      delay(3000)
+    )
   }
 }
