ddfe5fce3c952b2eaea08f8a5f7ad26740337459
akyrasouza <akyrapop@hotmail.com>
2022-01-17 12:05:29 -0300

    Adição de esquema sde visualização da Aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,47 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppMaterialModule } from './app-compartilhado/app-material/app-material.module';
+import { NavegacaoComponent } from './navegacao/navegacao.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavegacaoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    AppMaterialModule
+    AppMaterialModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+/*
+  BrowserModule:
+    Configura um aplicativo baseado em navegador
+    para fazer a transição de um aplicativo renderizado pelo servidor.
+
+  BrowsrAnimationsModule:
+    Exporta 'BrowserModule' com provedores adicionais [de injeção de dependência](guia/glossário#provider)
+    --para uso com animações.Ex: Spinner
+
+  HttpClienteModulo:
+    Configura suporte de proteção XSRF
+    para solicitações de saída.
+*/
