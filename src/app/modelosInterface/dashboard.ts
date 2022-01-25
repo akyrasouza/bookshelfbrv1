91ec86a4f6942909d8e9c3b0ad24ab67e7939b86
akyrasouza <akyrapop@hotmail.com>
2022-01-24 21:32:20 -0300

    Implementação dos Cards de Dashboard via Endpoint

--- src/app/modelosInterface/dashboard.ts
+++ src/app/modelosInterface/dashboard.ts
@@ -1,6 +1,12 @@
 export interface Dashboard {
-  title: string;
+  titulo: string;
+  subtilulo:string;
   img: string;
+  conteudo: string;
+  link:[{
+    ref:string,
+    texto: string
+  }];
   cols: number;
   rows: number;
 }
